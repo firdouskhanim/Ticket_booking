696604693d119f9e0eb3e5d43973718783e5946e
firdouskhanim <121568109+firdouskhanim@users.noreply.github.com>
2023-04-11 18:33:35 +0530

    Add files via upload

---
+++ movie ticket booking/main.py
@@ -0,0 +1,32 @@
+from ticketbooking import ticketinfo
+import sys
+class Main:
+    def execute(self,choice):
+        if choice == 1:
+            print("*******Show the seats*******")
+            movie_ticket_obj.show_seats()
+        if choice == 2:
+            print("*******Buy Ticket*******")
+            movie_ticket_obj.buy_ticket()
+        if choice == 3:
+            print("*******statistics*******")
+            movie_ticket_obj.statistics()
+        if choice == 4:
+            print("*******User Detail*******")
+            movie_ticket_obj.user_info()
+        if choice==0:
+           sys.exit()   
+            
+
+if __name__ == "__main__":
+    rows = int(input("Enter the number for rows : "))
+    columns = int(input("Enter the number of seats in each rows : "))
+
+    movie_ticket_obj = ticketinfo(rows,columns)
+    obj = Main()
+
+    while True:
+        choice = int(input(" 1.Show the seats \n2.Buy a ticket \n3.Statistics \n4.Show booked ticket's user info \n0.Exit : \n"))    
+        obj.execute(choice)                
+            
+            
